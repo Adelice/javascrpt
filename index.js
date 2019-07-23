81bff4962df47348d8f04c6af2078eae6c88c3db
Adelice <nancytoniw@gmail.com>
2019-07-23 12:10:41 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let name='Mosh';
+console.log(name);
+//cannot be a reserved keyword
+//
+let firstName='Mosh';
+let lastName='abby';
+const interestRate=0.3;
+let age=30;
+console.log(interestRate);
+let person={
+    name:'mosh',
+    age:20
+};
+person.name='john';
+console.log(person.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
